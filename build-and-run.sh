7c6b6f0f89c9995b8e51cd18a246da3da49f4873
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-14 16:03:00 +0100

    Sleep infinity when running
    This is a temporary workaround to troubleshoot docker compose dev containers. I need to find an approach to allow ./build-and-run to work for both outside and inside docker, and then also allow vs code to run sleep infinity so that the dev container never exits.

--- build-and-run.sh
+++ build-and-run.sh
@@ -6,7 +6,8 @@ if [[ -z "${DOCKER_FLAG_FOR_RUN_SCRIPT}" ]]; then
     docker compose --progress plain up
 else
     echo "Docker detected."
-    cd /app/src
+    sleep infinity
+    cd /app/src 
     cmake -DCMAKE_BUILD_TYPE=Release -S . -B /app/src/build
     cmake --build /app/src/build -j $(nproc) -v
     /app/src/build/tests/move_test
