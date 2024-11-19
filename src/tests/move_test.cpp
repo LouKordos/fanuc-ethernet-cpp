5e14cbc0f0b34bf17958cbed5764ddcae7fcf969
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-19 11:06:38 +0100

    Add test initialization

--- src/tests/move_test.cpp
+++ src/tests/move_test.cpp
@@ -1,6 +1,8 @@
-#include "../include/fanuc-ethernet-cpp.hpp"
+#include "../include/fanuc-ethernet.hpp"
+#include <print>
 
 int main() {
-    
+    fanuc_ethernet::FANUCRobot robot{"10.36.12.3"};
+    std::println("Robot object initialized");
     return 0;
 }
