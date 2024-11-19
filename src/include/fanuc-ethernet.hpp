301260677ec0f77cce00de851287252329f0d1eb
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-19 16:41:34 +0100

    Add function for reading current robot pose (turn, bitflip and E0-E3 not implemented yet)

--- src/include/fanuc-ethernet.hpp
+++ src/include/fanuc-ethernet.hpp
@@ -17,6 +17,24 @@ using namespace eipScanner::utils;
 // TODO: Include Tracy and measure send and connect times in detail, add message text to profiler metadata
 
 namespace fanuc_ethernet {
+    struct robot_pose {
+        int utool;
+        int uframe;
+        float x;
+        float y;
+        float z;
+        float yaw;
+        float pitch;
+        float roll;
+        uint8_t turn1;
+        uint8_t turn2;
+        uint8_t turn3;
+        uint8_t bitflip;
+        float E0;
+        float E1;
+        float E2;
+    };
+
     class FANUCRobot {
         private:
             std::string ip;
@@ -87,6 +105,96 @@ namespace fanuc_ethernet {
                 } catch (std::exception &e) {
                     std::cout << "Exception:" << e.what() << std::endl;
                     return std::unexpected{e.what()};
+                }
+            }
+
+            std::expected<float, std::string> parse_float_from_buffer(const std::vector<uint8_t> &buf, int offset) {
+                if(buf.size() < offset + sizeof(float)) {
+                    return std::unexpected{"Buffer too small to parse float"};
+                }
+
+                float temp;
+                std::memcpy(&temp, &buf[offset], sizeof(temp));
+                return temp;
+
+                // Risky due to alignment and endianness:
+                // return *reinterpret_cast<const float*>(&buf[offset]);
+            }
+
+            std::expected<robot_pose, std::string> parse_pose_buffer(const std::vector<uint8_t> &buf) {
+                ZoneScoped;
+                robot_pose pose;
+
+                pose.utool = buf[0] + buf[1] * 8;
+                pose.uframe = buf[2] + buf[3] * 8;
+                
+                int offset = 4;
+
+                auto result = parse_float_from_buffer(buf, offset);
+                if(!result.has_value()) {
+                    return std::unexpected{result.error()}; 
+                }
+                pose.x = result.value();
+                offset += sizeof(pose.x);
+
+                result = parse_float_from_buffer(buf, offset);
+                if(!result.has_value()) {
+                    return std::unexpected{result.error()}; 
+                }
+                pose.y = result.value();
+                offset += sizeof(pose.y);
+   
+                result = parse_float_from_buffer(buf, offset);
+                if(!result.has_value()) {
+                    return std::unexpected{result.error()}; 
+                }
+                pose.z = result.value();
+                offset += sizeof(pose.z);
+
+                result = parse_float_from_buffer(buf, offset);
+                if(!result.has_value()) {
+                    return std::unexpected{result.error()}; 
+                }
+                pose.yaw = result.value();
+                offset += sizeof(pose.yaw);
+
+                result = parse_float_from_buffer(buf, offset);
+                if(!result.has_value()) {
+                    return std::unexpected{result.error()}; 
+                }
+                pose.pitch = result.value();
+                offset += sizeof(pose.pitch);
+
+                result = parse_float_from_buffer(buf, offset);
+                if(!result.has_value()) {
+                    return std::unexpected{result.error()}; 
+                }
+                pose.roll = result.value();
+                offset += sizeof(pose.roll);
+
+                return pose;
+            }
+
+            std::expected<robot_pose, std::string> read_current_position() {
+                ZoneScoped;
+                if(!connected) {
+                    return std::unexpected{"Not connected yet."};
+                }
+                try {
+                    const CipUsint service_id = ServiceCodes::GET_ATTRIBUTE_SINGLE;
+                    const EPath epath(0x7D, 0x01, 0x01);
+
+                    auto response = message_router->sendRequest(session_info, service_id, epath);
+
+                    if (response.getGeneralStatusCode() == GeneralStatusCodes::SUCCESS) {
+                        Buffer buffer(response.getData());
+                        return parse_pose_buffer(buffer.data());
+                    }
+                    else {
+                        return std::unexpected{"Error: " + std::to_string(static_cast<int>(response.getGeneralStatusCode()))};
+                    }
+                } catch (std::exception &e) {
+                    return std::unexpected{"Exception:" + std::string{e.what()}};
                 }
             }
     };
--- src/tests/move_test.cpp
+++ src/tests/move_test.cpp
@@ -20,5 +20,19 @@ int main() {
             return 1;
         }
     }
-    return !robot.write_R_register(1, 69);
+    
+    auto success = robot.write_R_register(1, 69);
+    if(success) {
+        std::println("Position write succeeded.");
+    }
+    else {
+        std::println("Position write failed, exiting.");
+        return 1;
+    }
+
+    std::expected<fanuc_ethernet::robot_pose, std::string> result = robot.read_current_position();
+    if(result.has_value()) {
+        fanuc_ethernet::robot_pose pose = result.value();
+        std::println("Robot current position read suceeded: UTOOL={0}, UFRAME={1}, X={2}, Y={3}, Z={4}, Yaw={5}, Pitch={6}, Roll={7}", pose.utool, pose.uframe, pose.x, pose.y, pose.z, pose.yaw, pose.pitch, pose.roll);
+    }
 }
